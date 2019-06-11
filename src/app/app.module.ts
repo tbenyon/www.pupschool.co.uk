17e97647b34d2540d62723e7603150628b3aeb75
Tom Benyon <Tom.Benyon@creative-assembly.com>
2019-06-11 19:23:02 +0100

    Add logo icon to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from "ngx-bootstrap/collapse";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {CollapseModule} from "ngx-bootstrap/collapse";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {InlineSVGModule} from "ng-inline-svg";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    CollapseModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BsDropdownModule.forRoot(),
+        CollapseModule.forRoot(),
+        HttpClientModule,
+        InlineSVGModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
